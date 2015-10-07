dabb9bddd5bc8b22bc184577e1633cfcb0216798
klapnebojsa <klapnebojsa@gmail.com>
2015-10-08 00:44:53 +0200

    .

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cetiri
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cetiri/core_test.clj
@@ -0,0 +1,7 @@
+(ns cetiri.core-test
+  (:require [clojure.test :refer :all]
+            [cetiri.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
